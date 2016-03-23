d79a853baad6d4f1632b4b7b29b3a9ef5f76a467
bjorn3 <bjornbaron@outlook.com>
2016-03-23 17:00:01 +0100

    Initial content

---
+++ distance.js
@@ -0,0 +1,5 @@
+/* global Meteor, Template, Session, Router, AccountsTemplates, $ */
+
+Router.route("/distance", function(){
+    this.render("distance");
+});
--- goodgame_empire_helpers_meteor.js
+++ goodgame_empire_helpers_meteor.js
@@ -1,19 +1,52 @@
+/* global Meteor, Template, Session, Router, AccountsTemplates, $ */
+
+Router.configure({
+    layoutTemplate: "layout"
+});
+
+AccountsTemplates.configure({
+    defaultLayout: "layout"
+});
+
+Router.route("/", {
+    name: "home",
+    path: "/",
+    action: function(){
+        this.redirect("/distance");
+    }
+});
+
+Router.plugin('ensureSignedIn', {
+    except: ['home', 'atSignIn', 'atSignUp', 'atForgotPassword']
+});
+
+AccountsTemplates.configureRoute("signIn");
+
+Router.route("/logout", function(){
+    AccountsTemplates.logout();
+    this.redirect("/");
+});
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
+
+    Template.layout.onRendered(function(){
+        $(this.$[0]).foundation();
+    });
 }
 
 if (Meteor.isServer) {
