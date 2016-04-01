6c7612a9936b0e12743f61e8130c038c3740d121
bjorn3 <bjornbaron@outlook.com>
2016-04-01 19:50:55 +0200

    Add non working import functionality

--- goodgame_empire_helpers_meteor.js
+++ goodgame_empire_helpers_meteor.js
@@ -26,3 +26,19 @@ Router.route("/logout", function(){
     AccountsTemplates.logout();
     this.redirect("/");
 });
+
+Router.route("/import", function(){
+    if(this.params.query.json){
+        console.log(this.params.query.json);
+        try{
+            let json = JSON.parse(this.params.query.json);
+            console.log(json);
+            for(let castle of json){
+                Castles.insert(castle);
+            }
+        }catch(e){
+            console.log("invalid json");
+        }
+    }
+    this.render("import");
+});
