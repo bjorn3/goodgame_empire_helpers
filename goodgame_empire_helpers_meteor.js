f0de7f98b6af469518dc6ac750bccd545ff41567
bjorn3 <bjornbaron@outlook.com>
2016-04-01 19:35:31 +0200

    Remove standard code

--- goodgame_empire_helpers_meteor.js
+++ goodgame_empire_helpers_meteor.js
@@ -26,31 +26,3 @@ Router.route("/logout", function(){
     AccountsTemplates.logout();
     this.redirect("/");
 });
-
-if (Meteor.isClient) {
-    // counter starts at 0
-    Session.setDefault('counter', 0);
-
-    Template.hello.helpers({
-        counter: function () {
-            return Session.get('counter');
-        }
-    });
-
-    Template.hello.events({
-        'click button': function () {
-            // increment the counter when button is clicked
-            Session.set('counter', Session.get('counter') + 1);
-        }
-    });
-
-    Template.layout.onRendered(function(){
-        $(this.$[0]).foundation();
-    });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
